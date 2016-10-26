f665996c9138f0dcfc9a614f1a905da7a1ff18d8
James Gilliland <jgilliland@apqc.org>
2016-10-25 21:18:52 -0500

    Fix node clone warnings
    We know the helper recipes add this folder. The fall through is the only case
    where it might fail so ensure the directory exists there.

--- recipes/server.rb
+++ recipes/server.rb
@@ -68,16 +68,16 @@ when 'mac_os_x'
   include_recipe 'mariadb::_server_mac_os_x'
 when 'windows'
   include_recipe 'mariadb::_server_windows'
+else
+  # Makes absolutely sure this directory exists so the replication script installation below can't fail.
+  directory node['mariadb']['data_dir'] do
+    owner     'mysql'
+    group     'mysql'
+    action    :create
+    recursive true
+  end
 end
 
-# The data directory _should_ exist at this point. This makes absolutely
-# sure that it is, so the replication script installation below can't fail
-directory node['mariadb']['data_dir'] do
-  owner     'mysql'
-  group     'mysql'
-  action    :create
-  recursive true
-end
 
 template "#{node['mariadb']['data_dir']}/replication_master_script" do
   source 'replication_master_script.erb'
