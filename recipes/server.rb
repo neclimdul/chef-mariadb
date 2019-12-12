b236e40055d70ba5a2ad207641fd013a0dc858d7
James Gilliland <neclimdul@gmail.com>
2019-12-11 21:39:43 -0600

    try updating removed set_unless method
    Not sure if this is the correct method but it should let things run.

--- recipes/server.rb
+++ recipes/server.rb
@@ -35,9 +35,9 @@ if Chef::Config[:solo]
   end
 else
   # generate all passwords
-  node.set_unless['mariadb']['server_debian_password'] = secure_password
-  node.set_unless['mariadb']['server_root_password']   = secure_password
-  node.set_unless['mariadb']['server_repl_password']   = secure_password
+  node.default_unless['mariadb']['server_debian_password'] = secure_password
+  node.default_unless['mariadb']['server_root_password']   = secure_password
+  node.default_unless['mariadb']['server_repl_password']   = secure_password
   node.save
 end
 
